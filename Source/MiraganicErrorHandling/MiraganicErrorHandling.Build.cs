f03ef7014eaa6e766b72365dd2d6b5e78bd19530
Urgloshkin <Urgloshkin@users.noreply.github.com>
2023-06-23 15:09:55 -0600

    Update copyright headers for MIT license

--- Source/MiraganicErrorHandling/MiraganicErrorHandling.Build.cs
+++ Source/MiraganicErrorHandling/MiraganicErrorHandling.Build.cs
@@ -1,4 +1,9 @@
-﻿using System.IO;
+// Copyright 2023 Miraganic Studios.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+using System.IO;
 using UnrealBuildTool;
 
 public class MiraganicErrorHandling : ModuleRules
--- Source/MiraganicErrorHandlingEditor/Private/ECCustomization_ResultSwitchNode.cpp
+++ Source/MiraganicErrorHandlingEditor/Private/ECCustomization_ResultSwitchNode.cpp
@@ -1,4 +1,10 @@
-﻿// Copyright 2022 Miraganic Studios. All rights reserved.
+// Copyright 2023 Miraganic Studios.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
 
 
 #include "ECCustomization_ResultSwitchNode.h"
