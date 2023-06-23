fdcd357b2d118c8d99b3d699ab0a3bd1a533b8b4
Urgloshkin <Urgloshkin@users.noreply.github.com>
2023-06-23 15:27:12 -0600

    Fix copyright headers

--- Source/MiraganicErrorHandlingEditor/Private/ECAssetTypeActions_ErrorCategory.cpp
+++ Source/MiraganicErrorHandlingEditor/Private/ECAssetTypeActions_ErrorCategory.cpp
@@ -2,9 +2,6 @@
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
 
 
 #include "ECAssetTypeActions_ErrorCategory.h"
